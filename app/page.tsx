8002c084512996709252096da06c871bd2101daa
dalgoridim <b.fadamitan2019@gmail.com>
2025-04-22 01:20:03 +0100

    Update banner padding for improved layout consistency

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="div">
-      <div className="banner bg-black text-white md:min-h-dch size-full grid place-items-center p-4 sm:p-6 md:p-10">
+      <div className="banner bg-black text-white md:min-h-dch size-full grid place-items-center p-6 md:p-10">
         <div className="inner flex flex-wrap gap-4 md:gap-14 items-center justify-center">
           <div className="info md:space-y-6 space-y-4">
             <FadeInUp>
