9d9d29a1ac3c01c3d784d04f54f58cb7a4a9927b
dalgoridim <b.fadamitan2019@gmail.com>
2025-05-25 01:10:35 +0100

    + token

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -30,7 +30,7 @@ export default function AuthHandler() {
     };
 
     handleAuth();
-  }, [token, setUserToken, setRefreshPosts, router]);
+  }, [token]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-black px-4">
