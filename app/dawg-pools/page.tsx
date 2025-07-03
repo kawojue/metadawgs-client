ca91b8a5c5f531d28e4836412e7ac72179d1654f
Raheem Kawojue <kawojue08@gmail.com>
2025-07-03 01:14:05 +0100

    chore: remove everything about wallet

--- app/dawg-pools/page.tsx
+++ app/dawg-pools/page.tsx
@@ -1,38 +1,38 @@
-import AddressButton from "@/components/custom/AddressButton";
+// import AddressButton from "@/components/custom/AddressButton";
 import Pool from "@/components/custom/Pool";
 import { FadeInUp } from "@/components/custom/ScrollAnimation";
 import { pools } from "@/lib/data";
 
 export const metadata = {
-  title: "DawgPools",
-  description:
-    "Explore DawgPools, a multi-reward revenue-generating pool for MetaDawgs.",
+    title: "DawgPools",
+    description:
+        "Explore DawgPools, a multi-reward revenue-generating pool for MetaDawgs.",
 };
 
 function page() {
-  return (
-    <div className="bg-black text-white p-4 sm:p-6 md:p-10 space-y-8 md:space-y-16 min-h-dch">
-      <div className="flex flex-col gap-5 justify-center items-center">
-        <h1 className="title md:text-[76px] sm:text-6xl text-4xl tracking-[-2px] font-fredoka font-bold uppercase text-center">
-          Dawg Pools
-        </h1>
-        <p className="text-xl text-[#ACACAC] md:leading-[35px] max-w-[560px] text-center">
-          A multi-reward revenue-generating pool for metadawgs. Earn continuous
-          passive income for your holdings. Featuring multiple ecosystem staking
-          pools.
-        </p>
-        <AddressButton />
-      </div>
+    return (
+        <div className="bg-black text-white p-4 sm:p-6 md:p-10 space-y-8 md:space-y-16 min-h-dch">
+            <div className="flex flex-col gap-5 justify-center items-center">
+                <h1 className="title md:text-[76px] sm:text-6xl text-4xl tracking-[-2px] font-fredoka font-bold uppercase text-center">
+                    Dawg Pools
+                </h1>
+                <p className="text-xl text-[#ACACAC] md:leading-[35px] max-w-[560px] text-center">
+                    A multi-reward revenue-generating pool for metadawgs. Earn
+                    continuous passive income for your holdings. Featuring
+                    multiple ecosystem staking pools.
+                </p>
+                {/* <AddressButton /> */}
+            </div>
 
-      <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 md:gap-10 max-w-screen-lg mx-auto">
-        {pools.map((pool, index) => (
-          <FadeInUp key={index}>
-            <Pool pool={pool} />
-          </FadeInUp>
-        ))}
-      </div>
-    </div>
-  );
+            <div className="grid sm:grid-cols-2 grid-cols-1 gap-4 md:gap-10 max-w-screen-lg mx-auto">
+                {pools.map((pool, index) => (
+                    <FadeInUp key={index}>
+                        <Pool pool={pool} />
+                    </FadeInUp>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default page;
--- components/custom/AddressButton.tsx
+++ components/custom/AddressButton.tsx
@@ -1,159 +1,159 @@
-"use client";
+// "use client";
 
-import { ReactNode, useEffect } from "react";
-import { WalletIcon } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { Avatar, AvatarFallback } from "@/components/ui/avatar";
-import { hashAddress } from "@/lib/common";
-import { useWallet } from "@solana/wallet-adapter-react";
-import { useWalletModal } from "@solana/wallet-adapter-react-ui";
-import useAuth from "@/hooks/use-auth";
-import useMobileMenu from "@/hooks/use-mobile-menu";
-import { cn } from "@/lib/utils";
-import { postWithAuth } from "@/lib/api";
-import { toast } from "sonner";
+// import { ReactNode, useEffect } from "react";
+// import { WalletIcon } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+// import { hashAddress } from "@/lib/common";
+// import { useWallet } from "@solana/wallet-adapter-react";
+// import { useWalletModal } from "@solana/wallet-adapter-react-ui";
+// import useAuth from "@/hooks/use-auth";
+// import useMobileMenu from "@/hooks/use-mobile-menu";
+// import { cn } from "@/lib/utils";
+// import { postWithAuth } from "@/lib/api";
+// import { toast } from "sonner";
 
-function AddressButton({
-    label,
-    connectedLabel,
-    className,
-    activeClassName,
-    onConnect,
-    preventAutoClose = false,
-}: {
-    label?: string | ReactNode;
-    connectedLabel?: string | ReactNode;
-    className?: string;
-    activeClassName?: string;
-    onConnected?: () => void;
-    onConnect?: () => void;
-    preventAutoClose?: boolean;
-}) {
-    const { userToken, setUserProfile, userProfile } = useAuth();
-    const { setMenuIsOpen } = useMobileMenu();
+// function AddressButton({
+//     label,
+//     connectedLabel,
+//     className,
+//     activeClassName,
+//     onConnect,
+//     preventAutoClose = false,
+// }: {
+//     label?: string | ReactNode;
+//     connectedLabel?: string | ReactNode;
+//     className?: string;
+//     activeClassName?: string;
+//     onConnected?: () => void;
+//     onConnect?: () => void;
+//     preventAutoClose?: boolean;
+// }) {
+//     const { userToken, setUserProfile, userProfile } = useAuth();
+//     const { setMenuIsOpen } = useMobileMenu();
 
-    const { publicKey, disconnect } = useWallet();
-    const { setVisible } = useWalletModal();
+//     const { publicKey, disconnect } = useWallet();
+//     const { setVisible } = useWalletModal();
 
-    function connectWallet() {
-        if (!preventAutoClose) {
-            setMenuIsOpen(false);
-        }
+//     function connectWallet() {
+//         if (!preventAutoClose) {
+//             setMenuIsOpen(false);
+//         }
 
-        if (!userToken) {
-            toast(
-                "🧭 Wrong path, adventurer. Link your Twitter before summoning Connect Wallet."
-            );
-            return;
-        }
+//         if (!userToken) {
+//             toast(
+//                 "🧭 Wrong path, adventurer. Link your Twitter before summoning Connect Wallet."
+//             );
+//             return;
+//         }
 
-        onConnect?.();
+//         onConnect?.();
 
-        setTimeout(() => {
-            setVisible(true);
-            const checkConnection = setInterval(() => {
-                if (window.solana?.isConnected) {
-                    clearInterval(checkConnection);
-                    window.location.reload();
-                }
-            }, 1000);
-            setTimeout(() => clearInterval(checkConnection), 30000);
-        }, 500);
-    }
+//         setTimeout(() => {
+//             setVisible(true);
+//             const checkConnection = setInterval(() => {
+//                 if (window.solana?.isConnected) {
+//                     clearInterval(checkConnection);
+//                     window.location.reload();
+//                 }
+//             }, 1000);
+//             setTimeout(() => clearInterval(checkConnection), 30000);
+//         }, 500);
+//     }
 
-    function disconnectWallet() {
-        if (!preventAutoClose) {
-            setMenuIsOpen(false);
-        }
-        disconnect();
-    }
+//     function disconnectWallet() {
+//         if (!preventAutoClose) {
+//             setMenuIsOpen(false);
+//         }
+//         disconnect();
+//     }
 
-    useEffect(() => {
-        async function updateUserWallet() {
-            if (!publicKey || !userToken) return;
+//     useEffect(() => {
+//         async function updateUserWallet() {
+//             if (!publicKey || !userToken) return;
 
-            const currentWallet = publicKey.toBase58();
+//             const currentWallet = publicKey.toBase58();
 
-            try {
-                const response = await postWithAuth("/user/link-wallet", {
-                    walletAddress: currentWallet,
-                    approved: false,
-                });
+//             try {
+//                 const response = await postWithAuth("/user/link-wallet", {
+//                     walletAddress: currentWallet,
+//                     approved: false,
+//                 });
 
-                const data = response.data as {
-                    walletApproved: boolean;
-                };
+//                 const data = response.data as {
+//                     walletApproved: boolean;
+//                 };
 
-                if (userProfile) {
-                    setUserProfile({
-                        ...userProfile,
-                        user: {
-                            ...userProfile.user,
-                            walletApproved: data?.walletApproved || false,
-                        },
-                    });
-                }
-            } catch (error) {
-                console.error("Failed to link wallet:", error);
-                toast("Failed to link wallet wih account");
-            }
-        }
+//                 if (userProfile) {
+//                     setUserProfile({
+//                         ...userProfile,
+//                         user: {
+//                             ...userProfile.user,
+//                             walletApproved: data?.walletApproved || false,
+//                         },
+//                     });
+//                 }
+//             } catch (error) {
+//                 console.error("Failed to link wallet:", error);
+//                 toast("Failed to link wallet wih account");
+//             }
+//         }
 
-        updateUserWallet();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [publicKey, userToken]);
+//         updateUserWallet();
+//         // eslint-disable-next-line react-hooks/exhaustive-deps
+//     }, [publicKey, userToken]);
 
-    return (
-        <>
-            {!publicKey ? (
-                <Button
-                    className={cn(
-                        "bg-[#FFBE00] text-black rounded-full px-6! py-6! cursor-pointer hover:bg-[#FFBE00]/80!",
-                        className
-                    )}
-                    onClick={connectWallet}
-                >
-                    {!!label && label}
-                    {!label && (
-                        <>
-                            <WalletIcon size={12} />
-                            <span>Connect wallet</span>
-                        </>
-                    )}
-                </Button>
-            ) : (
-                <Button
-                    className={cn(
-                        "bg-[#FFBE00] text-black rounded-full px-3! py-6! cursor-pointer hover:bg-[#FFBE00]/80!",
-                        activeClassName
-                    )}
-                    onClick={disconnectWallet}
-                >
-                    {!connectedLabel && (
-                        <>
-                            <Avatar className="w-8 h-8 min-w-8 min-h-8">
-                                <AvatarFallback className="bg-gradient-to-r from-pink-500 to-purple-500" />
-                            </Avatar>
-                            <span>{hashAddress(publicKey.toBase58())}</span>
-                        </>
-                    )}
-                    {!!connectedLabel && connectedLabel}
-                    <svg
-                        width="15"
-                        height="14"
-                        viewBox="0 0 15 14"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            d="M7.00004 13.6666C3.31814 13.6666 0.333374 10.6818 0.333374 6.99992C0.333374 3.31802 3.31814 0.333252 7.00004 0.333252C9.18091 0.333252 11.1172 1.38044 12.3335 2.99941L10.5273 2.99946C9.58718 2.16991 8.35238 1.66659 7.00004 1.66659C4.05452 1.66659 1.66671 4.0544 1.66671 6.99992C1.66671 9.94545 4.05452 12.3333 7.00004 12.3333C8.35271 12.3333 9.58778 11.8297 10.528 10.9998H12.334C11.1177 12.6191 9.18118 13.6666 7.00004 13.6666ZM11.6667 9.66658V7.66658H6.33338V6.33325H11.6667V4.33325L15 6.99992L11.6667 9.66658Z"
-                            fill="#FF0000"
-                        />
-                    </svg>
-                </Button>
-            )}
-        </>
-    );
-}
+//     return (
+//         <>
+//             {!publicKey ? (
+//                 <Button
+//                     className={cn(
+//                         "bg-[#FFBE00] text-black rounded-full px-6! py-6! cursor-pointer hover:bg-[#FFBE00]/80!",
+//                         className
+//                     )}
+//                     onClick={connectWallet}
+//                 >
+//                     {!!label && label}
+//                     {!label && (
+//                         <>
+//                             <WalletIcon size={12} />
+//                             <span>Connect wallet</span>
+//                         </>
+//                     )}
+//                 </Button>
+//             ) : (
+//                 <Button
+//                     className={cn(
+//                         "bg-[#FFBE00] text-black rounded-full px-3! py-6! cursor-pointer hover:bg-[#FFBE00]/80!",
+//                         activeClassName
+//                     )}
+//                     onClick={disconnectWallet}
+//                 >
+//                     {!connectedLabel && (
+//                         <>
+//                             <Avatar className="w-8 h-8 min-w-8 min-h-8">
+//                                 <AvatarFallback className="bg-gradient-to-r from-pink-500 to-purple-500" />
+//                             </Avatar>
+//                             <span>{hashAddress(publicKey.toBase58())}</span>
+//                         </>
+//                     )}
+//                     {!!connectedLabel && connectedLabel}
+//                     <svg
+//                         width="15"
+//                         height="14"
+//                         viewBox="0 0 15 14"
+//                         fill="none"
+//                         xmlns="http://www.w3.org/2000/svg"
+//                     >
+//                         <path
+//                             d="M7.00004 13.6666C3.31814 13.6666 0.333374 10.6818 0.333374 6.99992C0.333374 3.31802 3.31814 0.333252 7.00004 0.333252C9.18091 0.333252 11.1172 1.38044 12.3335 2.99941L10.5273 2.99946C9.58718 2.16991 8.35238 1.66659 7.00004 1.66659C4.05452 1.66659 1.66671 4.0544 1.66671 6.99992C1.66671 9.94545 4.05452 12.3333 7.00004 12.3333C8.35271 12.3333 9.58778 11.8297 10.528 10.9998H12.334C11.1177 12.6191 9.18118 13.6666 7.00004 13.6666ZM11.6667 9.66658V7.66658H6.33338V6.33325H11.6667V4.33325L15 6.99992L11.6667 9.66658Z"
+//                             fill="#FF0000"
+//                         />
+//                     </svg>
+//                 </Button>
+//             )}
+//         </>
+//     );
+// }
 
-export default AddressButton;
+// export default AddressButton;
