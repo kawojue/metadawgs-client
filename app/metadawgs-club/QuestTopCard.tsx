3b5002f88f361162d8da0985b84d9431d023a380
Raheem Kawojue <kawojue08@gmail.com>
2025-07-13 01:44:14 +0100

    chore: turn off mindshare feature

--- app/metadawgs-club/QuestTopCard.tsx
+++ app/metadawgs-club/QuestTopCard.tsx
@@ -9,7 +9,7 @@ import { XOpenSignUpModal } from "@/lib/values";
 import { BellIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
-// import { toast } from "sonner";
+import { toast } from "sonner";
 import useLocalStorage from "use-local-storage";
 
 function QuestTopCard() {
@@ -67,8 +67,8 @@ function QuestTopCard() {
             return;
         }
 
-        router.push("/metadawgs-club/mindshare");
-        // toast("Coming Soon!");
+        toast("This feature is currently being upgraded to Dawghouse!");
+        // router.push("/metadawgs-club/mindshare");
     }
 
     if (isCreator) {
