b3e794255017f16ebe752c0080a1b02f8ea16889
Raheem Kawojue <kawojue08@gmail.com>
2025-07-13 02:17:47 +0100

    chore: turn off mindshare feature

--- app/metadawgs-club/QuestTopCard.tsx
+++ app/metadawgs-club/QuestTopCard.tsx
@@ -9,7 +9,7 @@ import { XOpenSignUpModal } from "@/lib/values";
 import { BellIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
-import { toast } from "sonner";
+// import { toast } from "sonner";
 import useLocalStorage from "use-local-storage";
 
 function QuestTopCard() {
