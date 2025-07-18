be6cd2e1533a0c35c4f886a56fd4f39c45fc4079
Raheem Kawojue <kawojue08@gmail.com>
2025-07-18 23:14:06 +0100

    chore: ignored more pages

--- app/MainLayout.tsx
+++ app/MainLayout.tsx
@@ -21,6 +21,9 @@ const IGNORED_ROUTE_PATTERNS = [
     /^\/auth/,
     /^\/collab-manager/,
     /^\/wherethemagicrestricted(\/.*)?$/,
+    /^\/leaderboard(\/.*)?$/,
+    /^\/dawgs-tge$/,
+    /^\/dawg-bot$/,
 ];
 
 function MainLayout({ children }: { children: ReactNode }) {
