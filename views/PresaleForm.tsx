2dab6d2dd42e1fde1dc00e90c6af2482b3aa3d70
Raheem Kawojue <kawojue08@gmail.com>
2025-08-30 20:53:15 +0100

    ..

--- views/PresaleForm.tsx
+++ views/PresaleForm.tsx
@@ -517,13 +517,12 @@ function PresaleForm({
                             type="button"
                             className="bg-[#FFBE00] text-black !py-6 rounded-full cursor-pointer disabled:cursor-not-allowed disabled:opacity-50!"
                             onClick={handlePurchase}
-                            // disabled={
-                            //     isLoading ||
-                            //     exchanging ||
-                            //     !canPurchase ||
-                            //     isPresaleClosed
-                            // }
-                            disabled={true}
+                            disabled={
+                                isLoading ||
+                                exchanging ||
+                                !canPurchase ||
+                                isPresaleClosed
+                            }
                         >
                             {isLoading ? (
                                 <>
